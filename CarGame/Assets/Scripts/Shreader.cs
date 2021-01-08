46c86b142f47e72ad3080f939d6eaf197c49e414
ayrton6119 <73109633+ayrton6119@users.noreply.github.com>
2021-01-08 10:15:50 +0100

    tried to fix things

---
+++ CarGame/Assets/Scripts/Shreader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shreader : MonoBehaviour
+{
+    //runs whenever other-Object triggers the shreadder
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        Destroy(otherObject.gameObject);
+    }
+
+}
